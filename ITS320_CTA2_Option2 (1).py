acecd35e962b7e8e6581af6baeb33a3763bc7744
pvelu1 <147877198+pvelu1@users.noreply.github.com>
2023-10-26 15:03:05 -0700

    Add files via upload

---
+++ ITS320_CTA2_Option2 (1).py
@@ -0,0 +1,13 @@
+#create empty dictionary
+car = {}
+
+#add entries
+car['car brand'] = input('Enter the car brand: \n')
+car['car model'] = input('Enter the car model: \n')
+car['car year'] = int(input('Enter the car year: \n'))
+car['car starting odometer'] = int(input('Enter the starting odometer reading: \n'))
+car['car ending odometer'] = int(input('Enter the ending odometer reading: \n'))
+car['estimated miles per gallon consumed'] = float(input('Enter the estimated miles per gallon consumed by the vehicle: \n'))
+
+#output dictionary
+print(car)
